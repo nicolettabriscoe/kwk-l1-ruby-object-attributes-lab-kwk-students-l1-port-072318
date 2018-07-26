057b6e59e2fa74af18b45c341c44f795707b774a
nicolettabriscoe <nicolettaskye@icloud.com>
2018-07-26 23:19:07 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
-# dog.rb
+class Dog
+  def name
+    @name
+  end
+  
+  def breed
+    @breed
+  end 
+  
+  def name = (name)
+    @name = name 
+  end
+  def breed = (breed)
+    @breed = breed
+  end
+
+end
+ 
+
+
+puppy = Dog.new("Benkei", "Pitbull")
+
+puppy.name
+puppy.breed
